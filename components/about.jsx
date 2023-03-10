ac86fe061239faab44e65d8f269c703139a54f02
khurram <khurram.ms1@gmail.com>
2023-03-10 15:35:45 +0500

    2

--- components/about.jsx
+++ components/about.jsx
@@ -4,8 +4,14 @@ import Heading from './heading'
 const About = () => {
   return (
     <>
-        <div className='flex px-4 py-10'>
+        <div className='flex flex-col px-4 py-10'>
             <Heading title="About Me" />
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-[60px]'>
+                <div>
+                    <img src="/picture.jpg" alt="Picture" width={300} />
+                </div>
+
+            </div>
         </div>
     </>
   )
