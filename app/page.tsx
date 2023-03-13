4d72232f93a8ddef7bb9b521e174138b0a7febab
khurram <khurram.ms1@gmail.com>
2023-03-13 14:38:08 +0500

    3

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,10 @@ import Navbar from '../components/navbar'
 import Hero from '../components/hero'
 import About from '../components/about'
 import Skills from '../components/skills'
+import Work from '../components/work'
+import Contact from '../components/contact'
+import Footer from '../components/footer'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -17,7 +21,12 @@ export default function Home() {
       <Hero/>
       <About  />
       <Skills/>
+      <Work/>
+      <Contact/>
     </main>
+    <footer>
+      <Footer/>
+    </footer>
 
     </>
   )
